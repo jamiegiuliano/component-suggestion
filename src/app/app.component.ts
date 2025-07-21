1414b75ebc06a47b045067f382554ca1786324d3
Jamie Giuliano <jgiuliano@onemedical.com>
2025-07-21 14:49:21 -0500

    First iteration working with components printing in the console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NovaLibModule } from '@visa/nova-angular';
+import { ComponentCategory, ComponentData, ComponentVariant } from './Component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,86 @@ import { NovaLibModule } from '@visa/nova-angular';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'component-suggestion';
+
+  // knownKeywords: { [key: string]: string[] } = {
+  //   components: [ "button" ],
+  //   keywords: ["primary", "secondary", "leading", "trailing", "disabled", "text", "icon" ]
+  // }
+
+  // Dictionary of component Data
+  componentData: ComponentData = {
+    button: {
+      "primary-text-button": {
+        label: "Primary Text Button",
+        keywords: [ "primary", "text" ],
+        html: "<button v-button>Primary action</button>",
+        overlap: 0
+      },
+      "primary-text-button-with-leading-icon": {
+        label: "Primary Text Button with Leading Icon",
+        keywords: [ "primary", "text", "leading", "icon" ],
+        html: "<button v-button v-icon-two-color><svg v-icon-visa-file-upload-tiny></svg>Primary action</button>",
+        overlap: 0
+      },
+      "primary-text-button-with-trailing-icon": {
+        label: "Primary Text Button with Trailing Icon",
+        keywords: [ "primary", "text", "trailing", "icon" ],
+        html: "<button v-button v-icon-two-color>Primary action<svg v-icon-visa-file-upload-tiny></svg></button>",
+        overlap: 0
+      }
+    }
+  };
+
+  getComponent(userInputKeywords: string[]): void {
+    var matchingComponentKeyword = userInputKeywords.find(keyword => Object.keys(this.componentData).includes(keyword)) ?? ""
+    
+    if(this.componentData[matchingComponentKeyword] !== undefined) {
+      let matchingComponentGroup: ComponentCategory = this.componentData[matchingComponentKeyword]
+
+      // find components with >1 matching keyword (current logic)
+      var componentVariants = Object.values(matchingComponentGroup)
+      var foundComponents = componentVariants.filter(componentMetaData => {
+        if(componentMetaData.keywords.some(kw => userInputKeywords.includes(kw))) {
+          return componentMetaData
+        }
+      })
+
+      // Update this to collect how many keywords matched.
+      // If none have a keyword count match (0), return all 
+      // If all matching have a keyword count match of 1, return all
+      // If any components have a keyword count match over 1, return all greater than 1
+
+      // var componentVariants = Object.values(matchingComponentGroup)
+
+      // componentVariants.map(component => {
+      //   const overlap = userInputKeywords.length + component.keywords.length - new Set(userInputKeywords.concat(component.keywords)).size;
+
+      //   component.overlap = overlap
+      //   console.log(component.overlap)
+      // })
+
+
+      var foundComponents = componentVariants.filter(componentMetaData => {
+        if(componentMetaData.keywords.some(kw => userInputKeywords.includes(kw))) {
+          return componentMetaData
+        }
+      })
+
+      let returnComponents = foundComponents.length > 0 ? foundComponents : componentVariants
+      console.log(returnComponents.map(component => component.label))
+      console.log(returnComponents.map(component => component.html))
+    }
+    
+  };
+
+  handleSubmit(userInput: HTMLTextAreaElement): void {
+    var userInputKeywords: string[] = userInput.value.toLowerCase().split(" ")
+    
+    this.getComponent(userInputKeywords)
+    // console.log(Object.values(this.componentData[userInputKeywords]).map(component => component.label ))
+    // console.log(Object.values(this.componentData[userInputKeywords]).map(component => component.html ))
+  };
 }
